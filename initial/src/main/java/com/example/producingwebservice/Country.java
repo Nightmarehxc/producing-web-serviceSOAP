becde81422dc1026889f7dfe1ecf9d62e03f6b01
Nightmarehxc <melopeico@gmail.com>
2021-01-20 13:58:04 +0100

    Initial commit

---
+++ initial/src/main/java/com/example/producingwebservice/Country.java
@@ -0,0 +1,140 @@
+
+package com.example.producingwebservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para country complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="country">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="population" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="capital" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="currency" type="{http://spring.io/guides/gs-producing-web-service}currency"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "country", namespace = "http://spring.io/guides/gs-producing-web-service", propOrder = {
+    "name",
+    "population",
+    "capital",
+    "currency"
+})
+public class Country {
+
+    @XmlElement(namespace = "http://spring.io/guides/gs-producing-web-service", required = true)
+    protected String name;
+    @XmlElement(namespace = "http://spring.io/guides/gs-producing-web-service")
+    protected int population;
+    @XmlElement(namespace = "http://spring.io/guides/gs-producing-web-service", required = true)
+    protected String capital;
+    @XmlElement(namespace = "http://spring.io/guides/gs-producing-web-service", required = true)
+    @XmlSchemaType(name = "string")
+    protected Currency currency;
+
+    /**
+     * Obtiene el valor de la propiedad name.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Define el valor de la propiedad name.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad population.
+     * 
+     */
+    public int getPopulation() {
+        return population;
+    }
+
+    /**
+     * Define el valor de la propiedad population.
+     * 
+     */
+    public void setPopulation(int value) {
+        this.population = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad capital.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCapital() {
+        return capital;
+    }
+
+    /**
+     * Define el valor de la propiedad capital.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCapital(String value) {
+        this.capital = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad currency.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Currency }
+     *     
+     */
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    /**
+     * Define el valor de la propiedad currency.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Currency }
+     *     
+     */
+    public void setCurrency(Currency value) {
+        this.currency = value;
+    }
+
+}
---
+++ initial/src/main/java/com/example/producingwebservice/GetCountryResponse.java
@@ -0,0 +1,64 @@
+
+package com.example.producingwebservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="country" type="{http://spring.io/guides/gs-producing-web-service}country"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "country"
+})
+@XmlRootElement(name = "getCountryResponse", namespace = "http://spring.io/guides/gs-producing-web-service")
+public class GetCountryResponse {
+
+    @XmlElement(namespace = "http://spring.io/guides/gs-producing-web-service", required = true)
+    protected Country country;
+
+    /**
+     * Obtiene el valor de la propiedad country.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Country }
+     *     
+     */
+    public Country getCountry() {
+        return country;
+    }
+
+    /**
+     * Define el valor de la propiedad country.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Country }
+     *     
+     */
+    public void setCountry(Country value) {
+        this.country = value;
+    }
+
+}
